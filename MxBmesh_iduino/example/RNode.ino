8de77059e7f24b763e4345124d26d12457301008
fredqian <fuqian@smeshlink.com>
2013-06-03 10:05:31 +0800

    add bmesh_iduino example

---
+++ MxBmesh_iduino/example/RNode.ino
@@ -0,0 +1,47 @@
+// Do not remove the include below
+#include "RNode.h"
+#include "MxBmesh.h"
+
+
+
+#ifndef LOCALADDRESS
+#define LOCALADDRESS	0
+#endif
+#define CHANNEL			26
+void myupload()
+{
+	MxBmesh.uploaddata(MSGTYPE_DU,"fuqian");
+}
+void mydataget(uint8_t *data,uint8_t len)
+{
+	Serial.write(data,len);
+
+}
+void myhanlder1()
+{
+	digitalWrite(30,!digitalRead(30));
+
+	MxBmesh.uploaddata(MSGTYPE_DU,"BUTTON1 PRESSED");
+}
+void myhanlder2()
+{
+	digitalWrite(30,!digitalRead(30));
+
+	MxBmesh.uploaddata(MSGTYPE_DU_NEEDACK,"BUTTON2 PRESSED");
+}
+void setup()
+{
+	pinMode(30,OUTPUT);
+	MxBmesh.begin(CHANNEL,LOCALADDRESS,15,38400,true);
+	MxBmesh.attachIntervalElapsed(myupload);
+	MxBmesh.attachDownloadData(mydataget);
+	attachInterrupt(0, myhanlder1, FALLING);
+	attachInterrupt(1, myhanlder2, FALLING);
+}
+
+void loop()
+{
+	MxBmesh.poll();
+	//clock_arch_sleepms(120);
+	//		_delay_ms(5);
+}
